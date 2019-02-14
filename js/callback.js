d5249a06ae4903a79d7ff5ff3b5270ab1e28fb13
Vishnu T <vthamiz5@gmail.com>
2019-02-14 17:31:21 +0530

    2

---
+++ js/callback.js
@@ -0,0 +1,12 @@
+
+
+func()
+
+function func(){
+
+    return console.log('alright then')
+}
+
+let fus=(a,b) => a+b;
+
+console.log(fus(1,2))
--- js/ob.js
+++ js/ob.js
@@ -1,5 +1,3 @@
-
-
 let srt = 123
 
 srt = String(srt)
@@ -15,15 +13,88 @@ console.log(2 ** 16 + ab)
 //alert('a'<'b')
 
 
-let asd = prompt('Name.. ? ')
+//let asd = prompt('Name.. ? ')
 
-alert(asd)
+//alert(asd)
 
-let ex = prompt('enter you exp' )
-let exp = (ex < 2) ? 'junior' : (ex < 4) ? 'senior': 'Experinced';
+//let ex = prompt('enter you exp' )
+//let exp = (ex < 2) ? 'junior' : (ex < 4) ? 'senior': 'Experinced';
 
-alert(exp)
+//alert(exp)
 
-let aw=prompt('Input')
+//let aw=prompt('Input')
 
-alert((aw>18)? 'dumb' : 'asol')
+//alert((aw>18)? 'dumb' : 'asol')
+
+let abc = 5
+
+while (abc < 6) {
+    console.log('Infinte')
+    abc++;
+}
+
+name: for (let a = 0; a < 3; a++) {
+    for (let b = 0; b < 3; b++) {
+
+        // var arr=prompt(`Enter the value (${a},${b})`)
+        if (1) {
+
+            break name;
+        }
+    }
+}
+
+//console.log(arr)
+
+
+
+let value = 100;
+let values = true;
+/*
+for (; values;) {
+       if(va>100){
+        valuse=false;
+    }
+} */
+
+function fun(values, val = 'dfs') {
+
+    return values + val;
+}
+
+//alert(fun('yo'))
+
+//let age = prompt('Enter the age', 0)
+
+function checkAge(age) {
+    return (age < 18) ? confirm('Denied! aren"t you....') : alert('allowed')
+}
+
+//checkAge(prompt('Value'))
+
+function pow(x,y){
+    return console.log(x**y)
+}
+
+pow(3,3)
+
+
+function callback(check,yes,no){
+
+     (check) ? false : yes ? true : nonFanNba;
+
+}
+
+function nba(){
+    confirm('Do you like Basketball.. ?')
+}
+
+function fanNba(){
+    alert('NBA ***')
+}
+
+function nonFanNba(){
+    alert('You are in the worng place kid...')
+}
+
+callback(nba(),fanNba(),nonFanNba())
