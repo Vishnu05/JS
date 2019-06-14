52b6c4ea801a89b4a6a02a9976f8feefe02864e5
Vishnu <vthamiz5@gmail.com>
2019-06-14 19:44:37 +0530

    Constructor

--- JS/Class.js
+++ JS/Class.js
@@ -13,6 +13,24 @@ js.Js()
 
 // var cls = new Cls()
 
+/** It literally work like java constructor */
 class Cls {
+    
+    /** This is how constructor works in js with constructor keyword */
+    constructor(name) {
+        console.log('constructor ' + name)
+    }
 
+    /** JS can have only one constructor */
+    /* constructor(name, cl) {
+        console.log('second constructor')
+     }*/
+
+    /** This doesn't work */
+    Cls() {
+        console.log('Default constructor type ...?')
+    }
 }
+
+let ob = new Cls('Vishnu')
+
