a6197630062176f07e2e9108a16c1de899740094
Vishnu <vthamiz5@gmail.com>
2019-07-04 19:59:18 +0530

    elements  creating

---
+++ JS/ES6/promises.js
@@ -0,0 +1,27 @@
+
+
+let check
+check = 1
+
+let promise = new Promise(function (reject, resolve){
+
+    if (check) {
+        resolve('promise and callbacks.....')
+    } else {
+        reject ('promise fails ')
+    }
+})
+
+console.log(promise)
+
+let promise1 = new Promise((resolve, reject) => {
+
+    setTimeout(() => {
+        resolve = {
+            json: "these are json values",
+            xml : '<xml are great for accessing data>'
+        }
+    }, 10)
+})
+
+console.log(promise1)
