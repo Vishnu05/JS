03acf36f9c4ba5f3e9b980b7fd3288e1b686cc9b
Vishnu <vthamiz5@gmail.com>
2019-07-16 19:15:42 +0530

    creating new element in dom nodes

--- JS/DOM/Elements.js
+++ JS/DOM/Elements.js
@@ -7,5 +7,29 @@ console.log('this is creating events in dom ~')
 let p = document.createElement('label')
 let pp = document.createTextNode('CreateElement')
 
+let nos = document.getElementById('cret')
+let eleCret = document.createElement('label')
+eleCret.textContent = 'New element is created...'
+console.log(eleCret)
+console.log(nos)
+nos.appendChild(eleCret)
 
+let ullist = document.getElementById('ullist')
+let adlist = document.createElement('li')
+adlist.textContent = 'List has been created successfully and added to dom node!!'
+ullist.appendChild(adlist)
+console.log(ullist)
 
+function tpe() {
+    let code1 = document.getElementById('code1')
+    console.log(code1.value)
+    let liss = document.createElement('li')
+    liss.textContent = code1.value
+    ullist.appendChild(liss)
+}
+
+ let keyBo = document.getElementById('keyBo')
+ console.dir("ji")
+
+ let adevn = document.getElementById('adeven')
+ adevn.addEventListener()
