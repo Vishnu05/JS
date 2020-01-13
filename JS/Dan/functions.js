bcbd6540052422970b290bce24718f048422ddd3
Vishnu <vthamiz5@gmail.com>
2020-01-13 19:17:51 +0530

    vim starting

---
+++ JS/Dan/functions.js
@@ -0,0 +1,28 @@
+
+
+function check(params) {
+
+
+    if (params ) {
+        console.log('true')
+    }
+
+    let car = {
+        petrol: 'paramshonda',
+        electric: 'tesla',
+        disel: 'toyoto',
+        paramscar: params.name
+    }
+
+    console.log(car)
+    console.log('hi vim I"m welcome you')
+    let editor = { hi: 'vim' }
+	editor.vim = 'terminal tool to execute or edit files'
+	editor.tool = 'bash and terminal'
+	editor.usage = 'only using terminal commands'
+    console.log(editor)
+    
+
+}
+
+check(1)
