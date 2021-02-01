b077a66ceb3ab45ff7104f145017271d01652283
Vishnu T <vishnuthamiz96@gmail.com>
2021-02-01 18:34:38 +0530

    ts

--- JS/DS/HashTable.js
+++ JS/DS/HashTable.js
@@ -1,2 +1,20 @@
 
 
+// finding out the occurance of the character, string or numbers in array
+
+let arr = [2, 4, 6, 3, 2, 2, 3]
+
+let map = new Map()
+for (i of arr) {
+
+    // checking if the map has key
+    if (map.has(i)) {
+        // if the key has update the value
+        map.set(i, map.get(i) + 1)
+    } else {
+        // insert the key and value
+        map.set(i, 1)
+    }
+}
+
+console.log('Map : ', map)
---
+++ TS/helloworld.ts
@@ -0,0 +1,26 @@
+
+
+var message: string = 'hello world 52'
+
+console.log(message)
+
+// assiging the types for the values 
+interface employee {
+    id: number
+    name: string
+    employementStatus: boolean
+}
+
+const Employee: employee = {
+    id: 3,
+    name: 'typescript',
+    employementStatus: true
+}
+
+let value = "name"
+
+console.log('%c hello css ', 'color: red')
+
+console.log('%c Hello world!',
+    ' color: tomato; font-size: 20px;')
+
