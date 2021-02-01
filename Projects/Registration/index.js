591d287d3f8cee34adf924e553bb4c4c1fee306e
Vishnu T <vishnuthamiz96@gmail.com>
2021-02-01 17:40:43 +0530

    nested array objects using map operator

--- Projects/Registration/index.js
+++ Projects/Registration/index.js
@@ -4,8 +4,21 @@ let username = document.getElementById('username')
 let userDisplay = document.getElementById('userDisplay')
 
 function user() {
-    
+
     let a = username.value
     userDisplay.innerHTML = a
     console.log('function call', a)
 }
+
+console.log('Hi hello')
+
+let ar = [
+    { id: 1, name: "Rome", country: "italy", aliveOrDead: true },
+    { id: 1, name: "NYC", country: "US", aliveOrDead: true },
+    { id: 1, name: "London", country: "UK", aliveOrDead: true },
+    { id: 1, name: "szhenchen", country: "China", aliveOrDead: true },
+]
+
+let mapVar = ar.map( (key, value)  => {
+    console.log(key, value)
+})
